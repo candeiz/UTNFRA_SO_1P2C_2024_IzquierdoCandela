d8ece761ea29bb803c508e9b2bcfbd8b16d8d4f0
candeiz <candeizquierdo15@.com>
2024-10-08 00:34:38 +0000

    Punto E y F

--- Punto_E.sh
+++ Punto_E.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd ..
-cd RTA_ARCHIVOS_Examen_20241007/
+cd RTA_ARCHIVOS_Examen_20241006/
 grep MemTotal /proc/meminfo > Filtro_Basico.txt
 sudo dmidecode -t chassis | grep 'Manufacturer' >> Filtro_Basico.txt
